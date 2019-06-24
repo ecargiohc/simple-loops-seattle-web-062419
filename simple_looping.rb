5f1a6aa9467a4bf34d83bd6fe012c28ce10ee7ba
Grace Colette <ilovegrace09@gmail.com>
2019-06-24 02:12:09 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -51,7 +51,7 @@ def for_iterator(number_of_times)
   for number_of_times in 0..7 do 
      puts "Welcome to Flatiron School's Web Development Course!"
    end
-   return "Welcome to Flatiron School's Web Development Course!"
+  "Welcome to Flatiron School's Web Development Course!"
  end
 # phrase = "Welcome to Flatiron School's Web Development Course!"
 
