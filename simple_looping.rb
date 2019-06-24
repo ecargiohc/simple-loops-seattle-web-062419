52cb0033a863d74890d96453bb75fe28c4478ad5
Grace Colette <ilovegrace09@gmail.com>
2019-06-24 02:10:23 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -48,7 +48,7 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-  for number_of_times 0..7 do 
+  for number_of_times in 0..7 do 
      puts "Welcome to Flatiron School's Web Development Course!"
    end
  end
